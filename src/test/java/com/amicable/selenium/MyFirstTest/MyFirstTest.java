be566d36ce64b00f7dae1837703dc771fd943257
VINCENT <vinamic@yahoo.com>
2022-12-24 11:51:06 +0000

    main

--- src/test/java/com/amicable/selenium/MyFirstTest/MyFirstTest.java
+++ src/test/java/com/amicable/selenium/MyFirstTest/MyFirstTest.java
@@ -1,10 +1,13 @@
 package com.amicable.selenium.MyFirstTest;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
@@ -12,47 +15,40 @@ import java.util.ArrayList;
 @RunWith(JUnit4.class)
 public class MyFirstTest {
 
+  ChromeDriver chromeDriver
    @Before
    public void setup(){
        System.setProperty("webdriver.chrome.driver", "");
-   }
+       chromeDriver.get("https://amaninja-concept2.myshopify.com/password");
 
+   }
         @Test
         public void runMyBrowser(){
-            ChromeDriver chromeDriver = new ChromeDriver();
+            chromeDriver = new ChromeDriver();
             chromeDriver.get("https://www.google.co.uk/");
 
-        }
+          @Test
+          public void myFirstTest() throws Exception {
+              WebElement titleElement = chromeDriver.findElement(By.xpath(xpathExpession: "/html"
+              String titleText = titleElement.getText();
 
+              Assert.assertEquals(titleText, "Amaninja-concept2");
 
-
-    @Test
-    public void myFirstTest() throws Exception {
-        ArrayList<String> numberArray = new ArrayList<String>();
-        numberArray.add("amicable");    //0
-        numberArray.add("two");    //1
-        numberArray.add("three");
-        numberArray.add("four");
-        numberArray.add("five");
-        numberArray.add("six");
-        numberArray.add("seven");
-        numberArray.add("eight");
-        numberArray.add("nine");   //8
-
-        System.out.println((numberArray.get(1)));
-
-      for (int i = 0; i < 10; i++){
-          System.out.println(i);
-
-          for (int i = 0; i < 9; i++ ){
-              System.out.println(numberArray.get(i));
-
-          }
-
-
-          Assert.assertEquals(numberArray.get(0), actual:"amicable");
+            @Test
+            public void myFirstTest() throws Exception {
+            WebElement passwordField= chromeDriver.findElement(By.id("password"));
+            passwordField.sendKeys("anything!!");
+            Thread.sleep(5000);
       }
 
-    }
+           @After
+           public void exitChrome(){
+                chromeDriver.quit();
 
-}
+                }
+
+
+ }
+
+
+
